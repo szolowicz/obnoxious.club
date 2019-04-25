082b3c2ddf9f4ec4f1bde555c42e9bdace2035a8
Lummit <lummit1337@gmail.com>
2019-04-25 12:35:41 +0200

    Changed commented line

--- assets/javascript/analytics.js
+++ assets/javascript/analytics.js
@@ -1,4 +1,4 @@
-// You can delete this.
+// Google Analytics JavaScript, you can delete this.
 !function(o,b,n,x,i,u,s){o.GoogleAnalyticsObject=n;o[n]||(o[n]=function(){
 (o[n].q=o[n].q||[]).push(arguments)});o[n].x=+new Date;u=b.createElement(x);
 s=b.getElementsByTagName(x)[0];u.src=i;s.parentNode.insertBefore(u,s)}
