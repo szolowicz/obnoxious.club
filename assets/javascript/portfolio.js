43b7d8661f0fbcca679e0dde0893ce8d40cebc96
Lummit <lummit1337@gmail.com>
2019-11-14 22:22:43 +0100

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -182,7 +182,7 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
     };
 
     (function () {
-        $.getJSON('https://ipgeolocation.com/', function (data) {
+        $.getJSON('http://ipgeolocation.com/?json=1', function (data) {
 
             writeLine(["Authenticating...", "Granting access to <span style='font-size: 14px; color: #06d;'>[unknown]</span>..."], 30, function () {
 
