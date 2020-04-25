d3a8e7b17b5787b358ca8f329e48217827746b60
Lummit <lummit1337@gmail.com>
2020-04-25 23:40:46 +0200

    New Geolocation API (again lol)

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -182,7 +182,7 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
     };
 
     (function () {
-        $.getJSON('http://ip-api.com/json/', function (data) {
+        $.getJSON('https://api.ipgeolocation.io/ipgeo?apiKey=bc46132fb5914b928799bfe862a1258d', function (data) {
 
             writeLine(["Authenticating...", "Granting access to <span style='font-size: 14px; color: #06d;'>[unknown]</span>..."], 30, function () {
 
@@ -193,8 +193,8 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
 
                 var usernames = ["user", "dude"];
 
-                writeLine(["Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>", "Welcome back, <i style='color: #0f0'>" +  ((data.query) ? data.query : usernames[Math.floor(Math.random()*usernames.length)]) 
-                    + "</i>! By the way, nice to see someone from " + ((data.country) ? data.country : 'your country') + " here!"], 30, 500, function () {
+                writeLine(["Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>", "Welcome back, <i style='color: #0f0'>" +  ((data.ip) ? data.ip : usernames[Math.floor(Math.random()*usernames.length)]) 
+                    + "</i>! By the way, nice to see someone from " + ((data.country_name) ? data.country_name : 'your country') + " here!"], 30, 500, function () {
 
                     if (app.skippedIntro)
                         return;
