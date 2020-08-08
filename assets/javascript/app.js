1da407bd0510824514c79bc91cdd29024ca6cce1
Lumm1t <lummit1337@gmail.com>
2020-08-09 00:46:01 +0200

    Change functions to arrow functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,27 +14,27 @@ const _app = function () {
   this.effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
   this.brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
 
-  this.titleChanger = function (text, delay) {
+  this.titleChanger = (text, delay) => {
     if (!text) return;
 
     delay = delay || 2000;
 
     let counter = 0;
 
-    setInterval(function () {
+    setInterval(() => {
       if (counter < text.length) document.title = text[counter++];
       else document.title = text[(counter = 0)];
     }, delay);
   };
 
-  this.iconChanger = function (urls, delay) {
+  this.iconChanger = (urls, delay) => {
     if (!urls) return;
 
     delay = delay || 2000;
 
     let counter = 0;
 
-    setInterval(function () {
+    setInterval(() => {
       if (counter < urls.length) {
         const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
 
--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -31,20 +31,20 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
 
 const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c7b5ef89f65c3faa1a';
 
-(function () {
+(() => {
   const timeouts = [];
 
-  window.mobileAndTabletCheck = function () {
+  window.mobileAndTabletCheck = () => {
     let check = false;
 
-    (function (a) {
+    ((a) => {
       if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
     })(navigator.userAgent || navigator.vendor || window.opera);
 
     return check;
   };
 
-  $(document).ready(function () {
+  $(document).ready(() => {
     const links = [
       {
         name: 'Lummit',
@@ -97,11 +97,11 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     app.audioElement.currentTime = $.cookie('videoTime');
   }
 
-  document.addEventListener('contextmenu', function (event) {
+  document.addEventListener('contextmenu', (event) => {
     event.preventDefault();
   });
 
-  document.body.onkeyup = function (event) {
+  document.body.onkeyup = (event) => {
     if (event.keyCode == 32 && app.skippedIntro) {
       if (app.backgroundToggler) {
         app.videoElement.play();
@@ -115,7 +115,7 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     }
   };
 
-  $('html').on('contextmenu', function (event) {
+  $('html').on('contextmenu', (event) => {
     const img = document.createElement('img');
 
     img.src = 'assets/others/trollface.jpg';
@@ -128,11 +128,11 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     document.body.appendChild(img);
   });
 
-  setInterval(function () {
+  setInterval(() => {
     $('.troll').remove();
   }, 600);
 
-  $('.skip').click(function () {
+  $('.skip').click(() => {
     skipIntro();
   });
 
@@ -140,7 +140,7 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     animateCss: function (animationName) {
       const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-      this.addClass('animated ' + animationName).one(animationEnd, function () {
+      this.addClass('animated ' + animationName).one(animationEnd, () => {
         $(this).removeClass('animated ' + animationName);
       });
 
@@ -148,10 +148,10 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     },
   });
 
-  const writeLine = function (text, speed, timeout, callback) {
+  const writeLine = (text, speed, timeout, callback) => {
     timeout = typeof timeout === 'number' ? timeout : [0, (callback = timeout)];
 
-    setTimeout(function () {
+    setTimeout(() => {
       const typed = new Typed('#line' + (app.id !== 2 ? ++app.id : (app.id += 2)), {
         strings: text,
         typeSpeed: speed,
@@ -160,28 +160,28 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     }, timeout);
   };
 
-  (function () {
-    $.getJSON(ipgeolocation, function (data) {
-      writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[unknown]</span>..."], 30, function () {
+  (() => {
+    $.getJSON(ipgeolocation, (data) => {
+      writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[unknown]</span>..."], 30, () => {
         if (app.skippedIntro) return;
 
         clearCursor();
 
         const usernames = ['user', 'dude'];
 
-        writeLine(["Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>", "Welcome back, <i style='color: #0f0'>" + (data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)]) + '</i>! By the way, nice to see someone from ' + (data.country_name ? data.country_name : 'your country') + ' here!'], 30, 500, function () {
+        writeLine(["Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>", "Welcome back, <i style='color: #0f0'>" + (data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)]) + '</i>! By the way, nice to see someone from ' + (data.country_name ? data.country_name : 'your country') + ' here!'], 30, 500, () => {
           if (app.skippedIntro) return;
 
           clearCursor();
 
-          writeLine(["<i style='color: #F62459'>obnoxious.club $$$</i>"], 120, 500, function () {
+          writeLine(["<i style='color: #F62459'>obnoxious.club $$$</i>"], 120, 500, () => {
             timeouts.push(
-              setTimeout(function () {
+              setTimeout(() => {
                 if (app.skippedIntro) return;
 
                 clearCursor();
 
-                setTimeout(function () {
+                setTimeout(() => {
                   skipIntro();
                 }, 500);
               }, 1000)
@@ -192,18 +192,18 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
     });
   })();
 
-  const skipIntro = function () {
+  const skipIntro = () => {
     if (app.skippedIntro) return;
 
     app.skippedIntro = true;
 
-    timeouts.forEach(function (timeout) {
+    timeouts.forEach((timeout) => {
       clearTimeout(timeout);
     });
 
     $('.top-right').remove();
 
-    $('#main').fadeOut(100, function () {
+    $('#main').fadeOut(100, () => {
       $('#main').remove();
 
       $('#marquee').marquee({
@@ -214,21 +214,21 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
         duplicated: true,
       });
 
-      setTimeout(function () {
+      setTimeout(() => {
         $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
       }, 200);
 
-      setTimeout(function () {
+      setTimeout(() => {
         const typed = new Typed('#brand', {
           strings: app.brandDescription,
           typeSpeed: 40,
-          onComplete: function () {
+          onComplete: () => {
             clearCursor();
           },
         });
       }, 1350);
 
-      setTimeout(function () {
+      setTimeout(() => {
         if (!app.shouldIgnoreVideo) {
           app.videoElement.play();
           app.audioElement.play();
@@ -236,7 +236,7 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
 
         app.videoElement.addEventListener(
           'timeupdate',
-          function () {
+          () => {
             $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
           },
           false
@@ -248,14 +248,14 @@ const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c
 
         $('.container').fadeIn();
 
-        $('.background').fadeIn(200, function () {
+        $('.background').fadeIn(200, () => {
           if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
         });
       }, 200);
     });
   };
 
-  const clearCursor = function () {
+  const clearCursor = () => {
     return $('span').siblings('.typed-cursor').css('opacity', '0');
   };
 })();
